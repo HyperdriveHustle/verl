2d5d893f23bba04da2be73af1d4dc998b70f9420
HyperdriveHustle <503147114@qq.com>
2025-11-24 08:31:43 +0000

    add script for 7b

--- verl/utils/tokenizer.py
+++ verl/utils/tokenizer.py
@@ -57,7 +57,7 @@ def hf_tokenizer(name_or_path, correct_pad_token=True, correct_gemma2=True, **kw
         )
         kwargs["eos_token"] = "<end_of_turn>"
         kwargs["eos_token_id"] = 107
-    max_tries = 5
+    max_tries = 10
     current_try = 0
     while current_try < max_tries:
         try:
